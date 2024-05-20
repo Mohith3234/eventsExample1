fa14dd9dfd037c04a3c99475b42acc087b60273b
Koninti Mohithkumar Goud <al6127@accionlabs.in>
2024-05-20 21:12:38 +0530

    first commit

---
+++ EventsExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+using ClassLibrary1;
+     class Program
+    {
+        static void Main()
+        {
+        SubcribedClass subcribedClass = new SubcribedClass();
+        PublishedClass publishedclass = new PublishedClass();
+        publishedclass.myEvent += subcribedClass.Add;
+        publishedclass.RaiseEvent(10, 20);
+        ReadKey();
+
+
+        }
+    }
+
---
+++ PublishedClass/PublishedClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    //delegate type
+    public delegate void MyDelegate(int x, int y);
+    public class PublishedClass
+    {
+        //private delegate
+        private MyDelegate mydelegate;
+        //step1:create the event 
+        public event MyDelegate myEvent
+        {
+            
+            add {mydelegate += value; }
+            remove {mydelegate -= value; }
+        }
+   
+        //step2:Raise Event
+    public void RaiseEvent(int a,int b)
+    {
+        if (this.mydelegate != null)
+        {
+            this.mydelegate(a, b);
+        }
+    }
+    }
+}
